f434359d3d8d296db5f5a845b2eb27d9e90aef39
Zheng Bao <fishbaoz@hotmail.com>
2016-12-13 13:36:14 +0800

    remove useless.

--- update.sh
+++
@@ -1,3 +0,0 @@
-
-grep --color "\<$1\>" . -r --exclude-dir=mysql --exclude-dir=.svn --exclude="*.po" --exclude="*.mo" --exclude-dir=doc --exclude-dir=locale
-echo "find -name \"*.php\" -exec sed -i 's/\<$1\>/weberp_$1/g' {} \;"
